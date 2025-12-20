d05333108c6ba5c872c299e6a335db685d15a3f4
shuyuye <shuyuyewang@gmail.com>
2025-12-20 13:40:37 +0800

    二分法找值或者找区间

---
+++ algo_for_cpp/35.search-insert-position.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size();
+        int mid = 0;
+        while(left < right){            
+            mid = left + (right - left)/2;
+            if(nums[mid]<target){
+                left = mid + 1;
+            }
+            else
+                right = mid;
+        }
+        return left;
+
+    }
+};
+// @lc code=end
+
