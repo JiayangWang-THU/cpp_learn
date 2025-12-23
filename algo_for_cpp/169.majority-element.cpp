366306b793116ab5590770f53b0f04658a8e7ff5
shuyuye <shuyuyewang@gmail.com>
2025-12-23 12:11:09 +0800

    <feat>:添加boyer投票法和中位数法（有序的数组很有用）

--- algo_for_cpp/169.majority-element.cpp
+++ algo_for_cpp/169.majority-element.cpp
@@ -8,15 +8,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> count_map;
-        for (int n : nums){
-            count_map[n]++;
-            if (count_map[n] > nums.size()/2){
-                return n;
-            }
-        }
-        return -1;
+     sort(nums.begin(), nums.end());
+     return nums[nums.size()/2];
     }
 };
 // @lc code=end
-
+//hash map
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> count_map;
+//         for (int n : nums){
+//             count_map[n]++;
+//             if (count_map[n] > nums.size()/2){
+//                 return n;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+// Boyer-Moore Voting Algorithm
+//   int count = 0;
+//        int candidate = 0;
+//        for (int n : nums){
+//             if(count == 0){
+//                 candidate = n;
+//             }
+//             count += (n == candidate) ? 1 : -1;
+//        }
+//        return candidate;
