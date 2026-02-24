c4fa1f306914ea5be07b26c18e81129b4244e715
shuyuye <shuyuyewang@gmail.com>
2026-02-24 09:00:51 +0800

    链表版两数和

---
+++ algo_for_cpp/2.add-two-numbers.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+            dummy = ListNode(0)
+            cur = dummy
+            carry = 0
+            
+            while l1 or l2 or carry:
+                x = l1.val if l1 else 0
+                y = l2.val if l2 else 0
+                
+                total = x + y + carry
+                carry = total // 10
+                
+                cur.next = ListNode(total % 10)
+                cur = cur.next
+                
+                if l1 is not None:
+                    l1 = l1.next
+                if l2 is not None: l2 = l2.next
+            
+            return dummy.next
+# @lc code=end
+
